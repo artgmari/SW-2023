645369fb726a2a3c7ac57e8ae1ea2b1e82e10062
projetoGmari <104384240+artgmari@users.noreply.github.com>
2023-04-05 13:37:36 -0300

    Add files via upload

---
+++ ExerciosAlunoMedia-main/ExercicioAlunoMedia/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExercicioAlunoMedia;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "Outro Aluno";
+        aluno1.nota1 = 4;
+        aluno1.nota2 = 5;
+
+        aluno1.mensagem();
+    }
+}
---
+++ ExerciosAlunoMedia-main/Programdesafio.cs
@@ -0,0 +1,19 @@
+﻿namespace ExercicioAlunoMedia;
+class Programdesafio
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno1 = new Aluno();
+        Console.Write("Digite seu nome:");
+        string? nome = Console.ReadLine();
+        Console.Write("Sua 1° nota:");
+        double nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite sua 2° nota:");
+        double nota2 = Convert.ToDouble(Console.ReadLine());
+        aluno1.nome = nome ="";
+        aluno1.nota1 = nota1;
+        aluno1.nota2 = nota2;
+
+        aluno1.mensagem();
+    }
+}
