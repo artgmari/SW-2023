0224693727463e30ccb7251282ce8c1c40d9dbb2
projetoGmari <104384240+artgmari@users.noreply.github.com>
2023-05-17 14:23:39 -0300

    Add files via upload

---
+++ exercio1/Animal.cs
@@ -0,0 +1,8 @@
+namespace exercio1;
+class Animal
+{
+    public virtual string Fala()
+    {
+        return "Som do";
+    }
+}
---
+++ exercio1/Cao.cs
@@ -0,0 +1,8 @@
+namespace exercio1;
+class Cao: Animal
+{
+    public override string Fala()
+    {
+        return base.Fala() + " Cão: au au";
+    }
+}
