0cc7364d31307c370dfb68932b1b768a8abd6e19
projetoGmari <104384240+artgmari@users.noreply.github.com>
2023-05-20 10:57:46 -0300

    Add files via upload

---
+++ TODOS  OS  EXEMPLOS/Exemplo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("DIGITE SEU NOME: ");
+        string? nome = Console.ReadLine();
+        Console.WriteLine("Seu nome é: "+nome);
+        Console.Write("Digite um número:");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("O número digitado foi:" + numero1);
+
+    }
+}
